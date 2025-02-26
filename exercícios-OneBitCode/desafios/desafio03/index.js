2c42543692d8ae00dd9776f6b42730e502d726b5
Robert Querino da Silva <164426066+RobertSilva65@users.noreply.github.com>
2025-02-25 22:41:16 -0300

    atualicacao
    exxs novos e desafios

---
+++ exercícios-OneBitCode/desafios/desafio03/index.js
@@ -0,0 +1,12 @@
+let pnome = window.prompt("Insira o seu primeiro nome")
+snome = window.prompt("Insira o seu Sobrenome")
+campo = window.prompt("Insira o seu Campo de estudo")
+data = window.prompt("Insira o seu Ano de nascimento")
+
+alert (
+
+  "Recruta cadastrado com sucesso!\n" + 
+  "\nNome completo " + pnome + " " + snome + 
+  "\nCampo de Estudo " + campo +
+  "\nIdade " + data
+)
---
+++ exercícios-OneBitCode/ex007/index.js
@@ -0,0 +1,12 @@
+let nome = "Robert"
+
+console.log(nome)
+console.error(nome)
+console.warn(nome)
+
+// window.alert(nome)
+window.alert(nome)
+nome = window.prompt("Insira o seu nome")
+let confirmacao = window.confirm("Comfirma essa acao?")
+                                                                                   
+console.log(nome)
