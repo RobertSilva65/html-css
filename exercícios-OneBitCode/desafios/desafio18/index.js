a1e1ae8a5823f29f939cc45a6896cef26b5542b0
Robert Querino da Silva <164426066+RobertSilva65@users.noreply.github.com>
2025-05-27 22:35:39 -0300

    att desafio 28

--- exercícios-OneBitCode/desafios/desafio18/index.js
+++ exercícios-OneBitCode/desafios/desafio18/index.js
@@ -15,19 +15,65 @@ function addPerson() {
 
   }
 
-  escalationteam.push(player)
+  const confirmation = confirm("Deseja confirmar a Escalação?")
 
-  const escalationSection = document.getElementById('escalation-list')
-  const playerList = document.createElement('p')
-  playerList.innerHTML = "Nome: " + nameSection + 
-  "<br><br>Posição: " + positionSection +
-  "<br><br>Número da Camisa: " + numberSection
-  escalationSection.appendChild(playerList)
+  if(confirmation) {
 
-  confirm("Deseja confirmar a Escalação?")
+    const escalationSection = document.getElementById('escalation-list')
+    const playerList = document.createElement('p')
+    playerList.innerHTML = "<br><br>Nome: " + nameSection + 
+    "<br><br>Posição: " + positionSection +
+    "<br><br>Número da Camisa: " + numberSection
+    escalationSection.appendChild(playerList)
+
+    escalationteam.push(player)
+    alert("Jogador escalado!")
+
+    document.getElementById('name').value = "";
+    document.getElementById('position').value = "";
+    document.getElementById('number').value = "";
+
+
+  }
 
   }else {
     alert("Preencha todos os campos")
+  }
+
+}
+
+function removePerson() {
+
+  const remove = parseFloat(prompt("Insira o número do jogador que Deseja remover?"))
+  const indice = escalationteam.findIndex(player => parseFloat(player.number) === remove);
+
+  if(indice !== -1) {
+
+    const confirmation = confirm("Deseja remover o jogador escolhido?")
+
+    if(confirmation){
+
+      escalationteam.splice(indice, 1);
+      const escalationSection = document.getElementById('escalation-list');
+      const players = escalationSection.getElementsByTagName('p');
+
+      for (let i = 0; i < players.length; i++) {
+      
+        if (players[i].innerHTML.includes(`Número da Camisa: ${remove}`)) {
+
+          escalationSection.removeChild(players[i]);
+          break
+
+        }
+
+      }
+
+    }
+    
+  }else {
+
+    alert("Jogador Não encontrado")
+
   }
 
 }
